1f3267421d0c696a4ad90eeb6ac4956a908ab4e4
dragonsky2357 <qkrdydals327@gmail.com>
2023-11-03 21:13:11 +0900

    fix: proxy middleware

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -4,7 +4,7 @@ module.exports = function (app: any) {
   app.use(
     "/api", // 불러오려는 server 의 api path
     createProxyMiddleware({
-      target: "http://localhost:8000", // server 주소를 넣어주면 된다.
+      target: `${process.env.REACT_APP_BASE_URL}`, // server 주소를 넣어주면 된다.
       changeOrigin: true,
     })
   );
